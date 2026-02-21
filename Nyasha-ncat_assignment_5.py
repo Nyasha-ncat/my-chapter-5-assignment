c0608341d48ffb120c8232e40b0d4e0c8c573a35
Nyasha-ncat <ntchimombe@aggies.ncat.edu>
2026-02-20 21:22:20 -0500

    Made my header

--- Nyasha-ncat_assignment_5.py
+++ Nyasha-ncat_assignment_5.py
@@ -1,1 +1,8 @@
-
+"""
+COMP 163 - Introduction to Programming
+Assignment: Chapter 5 - Loop Mastery
+Name: Nyasha Chimombe
+GitHub Username: Nyasha-ncat
+Date: 2/20/2025
+Description: This program demonstrates mastery of while, for, and nested loops through challanges.
+"""
