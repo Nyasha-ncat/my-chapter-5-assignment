a947652ac60dfe84340eaa968335b8fdc94530ff
Nyasha-ncat <ntchimombe@aggies.ncat.edu>
2026-02-20 21:30:41 -0500

    Implemented the Collatz sequence with while loop

--- Nyasha-ncat_assignment_5.py
+++ Nyasha-ncat_assignment_5.py
@@ -6,3 +6,21 @@ GitHub Username: Nyasha-ncat
 Date: 2/20/2025
 Description: This program demonstrates mastery of while, for, and nested loops through challanges.
 """
+
+
+# Collatz Sequence Generator
+current_number = int(input("Enter starting number: "))
+print(f"Sequence: {current_number}", end=" ")
+steps = 0
+while current_number != 1:
+    
+    if current_number % 2 == 0:
+        current_number = current_number // 2
+    else:
+        current_number = (current_number * 3) + 1
+    print(current_number, end=" ")
+    
+    steps += 1
+print() 
+print(f"Steps: {steps}")
+print() 
